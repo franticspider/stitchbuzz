eb1c23c62047051afdad4f1cfb3feea071007808
Simon Hickinbotham <sjh518@york.ac.uk>
2015-06-18 17:18:26 +0100

    initial coding and readme

--- src/stitchbuzz.c
+++ src/stitchbuzz.c
@@ -11,7 +11,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts(""); /* prints  */
+int main(int argc, char* argv[]) {
+
+	const int fnl = 1000,maxl=1000;
+	char line[maxl];
+	FILE *hfp,*pfp,*sfp;
+	char splitfn[fnl],popfn[fnl],sppfn[fnl];
+
+	const int sppstep = 10000000;	//TODO: read this from the config file
+	int lives = 8; 					//TODO: read this from the config file
+
+	printf("Analysing splitstory.dat\n");
+	sprintf(splitfn,"%s/splitstory.dat",argv[1]);
+	if((hfp=fopen(splitfn,"r"))==NULL){
+		printf("Unable to open file %s\n..exiting...\n",splitfn);
+		return 1;
+	}
+	else{
+		/* Let's count the lines */
+
+		while((fgets(line,maxl,fp))!=NULL){
+			lives++;
+		}
+		printf("Found %n runs - there should be this many popdy files\n");
+
+		//OK, now we can think about how to join...
+
+	}
+
+	/* We have to repair the numbering system for each `epoch'. Need to find a
+
+
 	return EXIT_SUCCESS;
 }
